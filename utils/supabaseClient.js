82c0d357b6bb7e97ac2a0d5cb73b4f8239289022
dillancdavids <137268080+dillancdavids@users.noreply.github.com>
2023-12-01 10:36:33 +0200

    changed login parameters

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -2,7 +2,7 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL ="https://yitliolscvqdehcsxkqz.supabase.co"
-const SUPABASE_ANNON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlpdGxpb2xzY3ZxZGVoY3N4a3F6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTEyNzEwNjYsImV4cCI6MjAwNjg0NzA2Nn0.dD85xBerc96yv2rOxk9wdXBzZmjei6LU026la0xKI7Q"
-
+   
+    const SUPABASE_URL ="https://hryyfxjhrolummzoeeyf.supabase.co"
+        const SUPABASE_ANNON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhyeXlmeGpocm9sdW1tem9lZXlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE0MTU0MTIsImV4cCI6MjAxNjk5MTQxMn0.zuiYaMCjrWUShkx3GnBEAuvr7KIrYlDOcQpgzOUFtBw"
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANNON_KEY)
